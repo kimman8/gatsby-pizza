c4b124964ce6fad15f8456affa748918091dfe63
Kim <kim_man@live.com>
2021-09-01 12:24:13 +1000

    dsf

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+<<<<<<< Updated upstream
   "component---src-pages-404-js": hot(preferDefault(require("/Users/KIM/Documents/react_projects/master-gatsby-master/starter-files/gatsby/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/KIM/Documents/react_projects/master-gatsby-master/starter-files/gatsby/src/pages/index.js"))),
   "component---src-pages-order-js": hot(preferDefault(require("/Users/KIM/Documents/react_projects/master-gatsby-master/starter-files/gatsby/src/pages/order.js"))),
@@ -12,5 +13,14 @@ exports.components = {
   "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/KIM/Documents/react_projects/master-gatsby-master/starter-files/gatsby/src/pages/slicemasters.js"))),
   "component---src-templates-pizza-js": hot(preferDefault(require("/Users/KIM/Documents/react_projects/master-gatsby-master/starter-files/gatsby/src/templates/Pizza.js"))),
   "component---src-templates-slicemaster-js": hot(preferDefault(require("/Users/KIM/Documents/react_projects/master-gatsby-master/starter-files/gatsby/src/templates/Slicemaster.js")))
+=======
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\pages\\index.js"))),
+  "component---src-pages-order-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\pages\\order.js"))),
+  "component---src-pages-pizzas-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\pages\\pizzas.js"))),
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\pages\\slicemasters.js"))),
+  "component---src-templates-pizza-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\templates\\Pizza.js"))),
+  "component---src-templates-slicemaster-js": hot(preferDefault(require("C:\\Users\\kim\\Desktop\\js\\gatsby pizza\\gatsby-pizza\\src\\templates\\Slicemaster.js")))
+>>>>>>> Stashed changes
 }
 
