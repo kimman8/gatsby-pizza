cf37f38608b02ea242f6d43153fa45abab998081
kim <kim_man@live.com>
2021-10-03 11:08:32 +1100

    dsf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,9 @@
-import React from "react";
-import ItemGrid from "../components/ItemGrid";
-import LoadingGrid from "../components/LoadingGrid";
-import { HomePageGrid } from "../styles/Grids";
-import useLatestData from "../utils/useLatestData";
-
-function CurrentlySlicing({ slicemasters }) {
-  return (
-    <div>
-      <h2 className="center">
-        <span className="mark tilt">Slice Masters On</span>
-      </h2>
-      <p>Standing by, ready to slice you up!</p>
-      {!slicemasters && <LoadingGrid count={4} />}
-      {slicemasters && !slicemasters?.length && (
-        <p>No one is working right now! </p>
-      )}
-      {slicemasters?.length && <ItemGrid items={slicemasters} />}
-    </div>
-  );
-}
-
-function HotSlices({ hotSlices }) {
-  return (
-    <div>
-      <h2 className="center">
-        <span className="mark tilt">Here's a small taste of what we do!</span>
-      </h2>
-      <p>come and visit us at the carrum downs shopping centre!</p>
-      {!hotSlices && <LoadingGrid count={4} />}
-      {hotSlices && !hotSlices?.length && <p>Nothin' in da case!</p>}
-      {hotSlices?.length && <ItemGrid items={hotSlices} />}
-    </div>
-  );
-}
+import React from 'react';
 
 export default function HomePage() {
-  const { slicemasters, hotSlices } = useLatestData();
   return (
     <div className="center">
-      <h1>The Best Chinese in Carrum Downs</h1>
-      <p>Open 10am to 6pm every single day!</p>
-      <HomePageGrid>
-        {/* <CurrentlySlicing slicemasters={slicemasters} /> */}
-        <HotSlices hotSlices={hotSlices} />
-      </HomePageGrid>
+      <h2>Your favourite Chinese joint in Carrum Downs since 1994!</h2>
     </div>
   );
 }
