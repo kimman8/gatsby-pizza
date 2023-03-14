06b81111eb37f83ad15f1ec7d1c21e09da62d921
kim <kim_man@live.com>
2023-03-14 17:11:04 +1100

    update lunch special prices

--- src/pages/lunch.js
+++ src/pages/lunch.js
@@ -31,8 +31,8 @@ export default function LunchSpecialPage() {
         <p>Choose up to 3 dishes with half fried rice or noodles</p>
       </LunchH2Styles>
       <LunchPriceStyles>
-        <button type='button'>S $11.90</button>
-        <button type='button'>L $12.90</button>
+        <button type='button'>S $12.90</button>
+        <button type='button'>L $13.90</button>
       </LunchPriceStyles>
       <ImageStyles>
         {/* <img src={lunch} alt="lunch special" /> */}
